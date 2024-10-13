786d309e515075a6d981c41f581aebd97e81eb49
duythanh7903 <beedyto@gmail.com>
2024-10-13 21:06:15 +0700

    connect application to database

---
+++ src/configs/database.js
@@ -0,0 +1,11 @@
+
+import sql from "mysql2/promise"
+
+const pool = sql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    database: 'virgo_school',
+    port: '3307'
+})
+
+export default pool
--- src/controllers/web/controller.student.js
+++ src/controllers/web/controller.student.js
@@ -1,4 +1,11 @@
-const getTestPage = (req, res) => {
+import pool from '../../configs/database'
+
+const getTestPage = async (req, res) => {
+    const sql = "select * from students order by student_created_at desc"
+    const [rows, fields] = await pool.execute(sql)
+
+    console.log(`Row: ${rows}`)
+
     return res.render('test/test.ejs')
 }
 
